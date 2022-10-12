277c5ff616f95ba4764e54511275f8fec08bb4a9
Rafael <grafa74@gmail.com>
2022-10-12 20:39:01 +0200

    trying to merge and close

--- testing/sample.js
+++ testing/sample.js
@@ -11,6 +11,25 @@ console.log(firstElement);
 // expected output: 1
 */
 export function myShift(array) {
-    let newArray = array.shift();
+    const [removed, ...newArray] = array;
+    console.log(removed);
     return newArray;
 }
+
+export function myPush(array, element) {
+    let newElement = element;
+    let newArray = [newElement, ...array];
+    let newArrayLength = newArray.length;
+    console.log(newArrayLength);
+    return newArray;
+}
+/*
+The push() method adds one or more elements to the end of an array and returns the new length of the array. 
+const animals = ['pigs', 'goats', 'sheep'];
+
+const count = animals.push('cows');
+console.log(count);
+// expected output: 4
+console.log(animals);
+// expected output: Array ["pigs", "goats", "sheep", "cows"]
+*/
